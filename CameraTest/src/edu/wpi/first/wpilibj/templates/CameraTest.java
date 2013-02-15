15ab971bda37a706e89b443de3e6f0ea5f1b8734
Francesco Macagno <fjmacagno1@yahoo.com>
2013-02-14 18:10:51 -0800

    Adding camera tracking in teleop

--- CameraTest/src/edu/wpi/first/wpilibj/templates/CameraTest.java
+++ CameraTest/src/edu/wpi/first/wpilibj/templates/CameraTest.java
@@ -39,12 +39,15 @@ public class CameraTest extends SimpleRobot {
     
     AxisCamera camera;          // the axis camera object (connected to the switch)
     CriteriaCollection cc;      // the criteria for doing the particle filter operation
+	private Drivetrain2 drivetrain;
+    private final static double TRACKING_ERROR = .05;
     
     public void robotInit() {
         camera = AxisCamera.getInstance();  // get an instance ofthe camera
         cc = new CriteriaCollection();      // create the criteria for the particle filter
         cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false);
         cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false);
+        drivetrain = new Drivetrain2();
     }
 
     public void autonomous() {
@@ -95,8 +98,77 @@ public class CameraTest extends SimpleRobot {
      */
     public void operatorControl() {
         while (isOperatorControl() && isEnabled()) {
+        	
+                ParticleAnalysisReport r = track(null);
+                
+                boolean move = false;
+                
+                if(r != null){
+                	
+                	//int off = r.imageWidth - r.center_mass_x;
+                	
+                	if(Math.abs(r.center_mass_x_normalized) < TRACKING_ERROR ){
+                		//TODO: Shoot
+                		drivetrain.drive(0,0);
+                	} else if(move) drivetrain.drive(r.center_mass_x_normalized,r.center_mass_x_normalized);
+                	
+                	System.out.println(r.center_mass_x_normalized);
+                	
+                }
+
             Timer.delay(1);
         }
     }
+    
+	public ParticleAnalysisReport track(String file){
+		try {
+            /**
+             * Do the image capture with the camera and apply the algorithm described above. This
+             * sample will either get images from the camera or from an image file stored in the top
+             * level directory in the flash memory on the cRIO. The file name in this case is "10ft2.jpg"
+             * 
+             */
+            ColorImage image = (file != null ? new RGBImage(file) : camera.getImage());
+            BinaryImage thresholdImage = image.thresholdRGB(0, 45, 25, 255, 0, 47);   // keep only green objects
+            BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 2);  // remove small artifacts
+            BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
+            BinaryImage filteredImage = convexHullImage.particleFilter(cc);// find filled in rectangles
+            
+            ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
+            
+            /*for (int i = 0; i < reports.length; i++) {                                // print results
+                ParticleAnalysisReport r = reports[i];
+                System.out.println("Particle: " + i + ":  Center of mass x: " + r.center_mass_x);
+            }
+            System.out.println(filteredImage.getNumberParticles() + "  " + Timer.getFPGATimestamp());*/
+            
+            ParticleAnalysisReport close = null;
+            for (int i = 0; i < reports.length; i++) {
+                if(close == null)
+                	close = reports[i];
+                else {
+                	if(close.center_mass_x_normalized > reports[i].center_mass_x_normalized)
+                		close = reports[i];
+                }
+            }
+            
+            filteredImage.free();
+            convexHullImage.free();
+            bigObjectsImage.free();
+            thresholdImage.free();
+            image.free();
+            
+            return close;
+            
+        } catch (NIVisionException ex) {
+            ex.printStackTrace();
+        } catch (AxisCameraException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
 }
         
