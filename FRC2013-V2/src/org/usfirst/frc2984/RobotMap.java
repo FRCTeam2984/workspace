e11074806bc036771487889a44c4aea0870e56a9
Francesco Macagno <fjmacagno1@yahoo.com>
2013-02-15 21:27:37 -0800

    Chnaged numbers again

--- FRC2013-V2/src/org/usfirst/frc2984/RobotMap.java
+++ FRC2013-V2/src/org/usfirst/frc2984/RobotMap.java
@@ -14,7 +14,7 @@ class RobotMap {
     public static final int LEFT2 = 5;
     public static final int RIGHT1 = 6;
     public static final int RIGHT2 = 7;
-    public static final int SHOOTER1 = 7;
+    public static final int SHOOTER1 = 9;
     public static final int SHOOTER2 = 8;
-	public static final int INSERTER = 5;
+	public static final int INSERTER = 1;
 }
