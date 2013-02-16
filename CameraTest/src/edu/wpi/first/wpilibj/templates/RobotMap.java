a344a6f47109dc057ab95f92efd5595d0dd48f28
Francesco Macagno <fjmacagno1@yahoo.com>
2013-02-16 11:58:31 -0800

    Added two classes

---
+++ CameraTest/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author jason
+ */
+class RobotMap {
+    //Jaguars
+    public static final int LEFT1 = 9;
+    public static final int LEFT2 = 10;
+    public static final int RIGHT1 = 1;
+    public static final int RIGHT2 = 2;
+    public static final int SHOOTER1 = 7;
+    public static final int SHOOTER2 = 8;
+    public static final int BANDS = 3;
+	public static final int INSERTER = 5;
+}
